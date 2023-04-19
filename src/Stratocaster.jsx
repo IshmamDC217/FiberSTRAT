2fc10c946d5cf9e5cc21c197ffed4548e2746df1
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-04-19 03:17:19 +0100

    Completed FiberSTRAT

--- src/Stratocaster.jsx
+++ src/Stratocaster.jsx
@@ -1,22 +1,42 @@
-import { Environment, OrbitControls, useGLTF, Text, useFont } from '@react-three/drei'
+import { Environment, OrbitControls, useGLTF, Text } from '@react-three/drei';
 
 export default function Stratocaster() {
     const strat = useGLTF('./JFstratocaster.glb');
-    const font = useFont('Windsong_Regular.json');
 
-    console.log(font)
     return (
         <>
             <OrbitControls />
-
             <primitive
                 object={strat.scene}
                 position={[3, -1.8, 1]}
                 rotation={[1.6, 0, 0.8]}
             />
 
-            <Environment preset='warehouse' />
+            <group position={[-3, 1, 0]}>
+                <Text
+                    font={'Arial'}
+                    color='black'
+                    fontSize={0.5}
+                    position={[0, 0, 0]}
+                    rotation={[0, 0, 0]}
+                >
+                    FENDER STRATOCASTER
+                </Text>
 
+                <Text
+                    font={'Arial'}
+                    color='black'
+                    fontSize={0.3}
+                    position={[0.2, -2, 0]}
+                    rotation={[0, 0, 0]}
+                    maxWidth={6}
+                    lineHeight={1}
+                >
+                    The Fender Stratocaster is the quintessential guitar that has stood the test of time, loved by generations of guitarists for its versatility and iconic design. From Jimi Hendrix and Eric Clapton to John Mayer and Ed Sheeran, the Stratocaster has been the weapon of choice for some of the most legendary guitarists of all time. Its signature sound, playability, and stylish aesthetics make it the perfect guitar for any genre of music.
+                </Text>
+            </group>
+
+            <Environment preset='warehouse' />
         </>
     );
 }
